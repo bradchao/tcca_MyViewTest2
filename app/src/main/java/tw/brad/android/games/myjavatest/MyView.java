3dd600eb5278618f6d05776140276fb813b10f06
Brad <brad@brad.tw>
2017-08-02 20:23:52 +0800

    2017080202

--- app/src/main/java/tw/brad/android/games/myjavatest/MyView.java
+++ app/src/main/java/tw/brad/android/games/myjavatest/MyView.java
@@ -1,6 +1,9 @@
 package tw.brad.android.games.myjavatest;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -11,12 +14,22 @@ import android.view.View;
  */
 
 public class MyView extends View {
+    private Resources res;
+
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        //setBackgroundResource(R.drawable.mybg);
+        res = context.getResources();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        Bitmap bg = BitmapFactory.decodeResource(res,R.drawable.mybg);
+
+        canvas.drawBitmap(bg, 0, 0, null);
+        
+
     }
 }
