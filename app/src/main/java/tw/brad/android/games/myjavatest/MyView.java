c569f49958a2ffc59aac7395071bab35e3bd3e7b
Brad <brad@brad.tw>
2017-08-02 20:57:00 +0800

    2017080205

--- app/src/main/java/tw/brad/android/games/myjavatest/MyView.java
+++ app/src/main/java/tw/brad/android/games/myjavatest/MyView.java
@@ -5,6 +5,7 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -19,20 +20,28 @@ public class MyView extends View {
     private Bitmap ballBmp;
     private int viewW, viewH;
     private boolean isInit;
+    private float ballW, ballH;
+    private Matrix matrix;
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         setBackgroundResource(R.drawable.mybg);
         res = context.getResources();
         ballBmp = BitmapFactory.decodeResource(res,R.drawable.ball);
-
+        matrix = new Matrix();
     }
 
     private void init(){
         isInit = true;
         viewW = getWidth(); viewH = getHeight();
 
-
+        ballW = viewW / 2f; ballH = ballW;
+        matrix.reset();
+        matrix.postScale(ballW / ballBmp.getWidth(),
+                ballH / ballBmp.getHeight());
+        ballBmp = Bitmap.createBitmap(
+                ballBmp,0,0,ballBmp.getWidth(),ballBmp.getHeight(),
+                matrix, false);
 
 
     }
@@ -41,7 +50,6 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if (!isInit) init();
-
 
         canvas.drawBitmap(ballBmp, 0, 0, null);
 
