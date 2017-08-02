ad4a157ab9d5fbd939f49ca0c0493ff9fccfc3e4
Brad <brad@brad.tw>
2017-08-02 19:50:29 +0800

    2017080201

---
+++ app/src/main/java/tw/brad/android/games/myjavatest/MainActivity.java
@@ -0,0 +1,20 @@
+package tw.brad.android.games.myjavatest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/tw/brad/android/games/myjavatest/MyView.java
@@ -0,0 +1,22 @@
+package tw.brad.android.games.myjavatest;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/8/2.
+ */
+
+public class MyView extends View {
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
