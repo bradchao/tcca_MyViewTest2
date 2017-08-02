39f17a4e552f37deabb255cffdf224ef19677e9c
Brad <brad@brad.tw>
2017-08-02 20:42:09 +0800

    2017080204

--- app/src/main/java/tw/brad/android/games/myjavatest/MyView.java
+++ app/src/main/java/tw/brad/android/games/myjavatest/MyView.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -16,17 +17,31 @@ import android.view.View;
 public class MyView extends View {
     private Resources res;
     private Bitmap ballBmp;
+    private int viewW, viewH;
+    private boolean isInit;
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         setBackgroundResource(R.drawable.mybg);
         res = context.getResources();
         ballBmp = BitmapFactory.decodeResource(res,R.drawable.ball);
+
+    }
+
+    private void init(){
+        isInit = true;
+        viewW = getWidth(); viewH = getHeight();
+
+
+
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        if (!isInit) init();
+
 
         canvas.drawBitmap(ballBmp, 0, 0, null);
 
